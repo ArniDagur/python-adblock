83d10fc33e283c40e93f2a94c233fe7a663cc865
ArniDagur <ArniDagur@users.noreply.github.com>
2020-06-08 03:03:22 +0000

    deploy: 26b2ffa2e7d1970c30af5d9e97530575916fe769

--- docs/source-files.js
+++ docs/source-files.js
@@ -80,5 +80,5 @@ sourcesIndex["unicode_bidi"] = {"name":"","dirs":[{"name":"char_data","files":["
 sourcesIndex["unicode_normalization"] = {"name":"","files":["__test_api.rs","decompose.rs","lib.rs","lookups.rs","normalize.rs","perfect_hash.rs","quick_check.rs","recompose.rs","stream_safe.rs","tables.rs"]};
 sourcesIndex["unicode_xid"] = {"name":"","files":["lib.rs","tables.rs"]};
 sourcesIndex["unindent"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["url"] = {"name":"","files":["encoding.rs","form_urlencoded.rs","host.rs","lib.rs","origin.rs","parser.rs","path_segments.rs","quirks.rs","slicing.rs"]};
+sourcesIndex["url"] = {"name":"","files":["form_urlencoded.rs","host.rs","lib.rs","origin.rs","parser.rs","path_segments.rs","query_encoding.rs","quirks.rs","slicing.rs"]};
 createSourceSidebar();
