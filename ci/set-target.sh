602fd530c827a553d3630fb867a19e129b84f9fd
Árni Dagur <arni@dagur.eu>
2021-01-26 21:05:30 +0000

    Make Windows use bash for setting environment variable

--- ci/set-target.sh
+++ ci/set-target.sh
@@ -8,22 +8,28 @@ fi
 os=$1
 architecture=$2
 
+output() {
+    echo "Setting ACTIONS_RUST_TARGET=$1"
+    echo "ACTIONS_RUST_TARGET=$1" >> "$GITHUB_ENV"
+    exit 0
+}
+
 if [ "$architecture" = "x64" ]; then
     if [ "$os" = "ubuntu-latest" ]; then
-        echo "x86_64-unknown-linux-gnu"
+        output "x86_64-unknown-linux-gnu"
     elif [ "$os" = "macos-latest" ]; then
-        echo "x86_64-apple-darwin"
+        output "x86_64-apple-darwin"
     elif [ "$os" = "windows-latest" ]; then
-        echo "x86_64-pc-windows-msvc"
+        output "x86_64-pc-windows-msvc"
     else
         echo "Unknown 64-bit OS: $os"
         exit 1
     fi
 elif [ "$architecture" = "x86" ]; then
     if [ "$os" = "ubuntu-latest" ]; then
-        echo "i686-unknown-linux-gnu"
+        output "i686-unknown-linux-gnu"
     elif [ "$os" = "windows-latest" ]; then
-        echo "i686-pc-windows-msvc"
+        output "i686-pc-windows-msvc"
     else
         echo "Unknown 32-bit OS: $os"
         exit 1
