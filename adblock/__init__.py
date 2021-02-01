7fec21d6883453f4e26ce955efb87bc43b070935
Árni Dagur <arni@dagur.eu>
2021-02-01 16:33:26 +0000

    Try removing relative import in __init__.py

--- adblock/__init__.py
+++ adblock/__init__.py
@@ -1,4 +1,4 @@
-from .adblock import __version__, Engine, FilterSet, BlockerResult, UrlSpecificResources
+from adblock.adblock import __version__, Engine, FilterSet, BlockerResult, UrlSpecificResources
 
 
 __all__ = ("Engine", "FilterSet", "BlockerResult", "UrlSpecificResources")
